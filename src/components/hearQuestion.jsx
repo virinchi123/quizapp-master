6bff2aa8c631dd2bdd693dc73883d651300e1738
virinchi123 <raovirinchi123@gmail.com>
2020-10-09 11:55:14 +0530

    modified styles

--- src/components/hearQuestion.jsx
+++ src/components/hearQuestion.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../styles/QuestionPage.css';
 import Speech from 'react-speech';
+import classes from '../styles/hearQuestion.module.css';
 
 const Hearing = function(props){
 
     let { question, option1, option2, option3, setAnswer } = props;
     
     let text = question?question.substring(question.lastIndexOf(',')+2):'';
+    let optionsClasses = [classes.options, 'options'].join(' ');
+    let optionClasses = [classes.option, "btn btn-outline-primary btn-option"].join(' ');
 
     return(
         <div className="Question">
@@ -20,15 +23,15 @@ const Hearing = function(props){
             <form className="options">
                 <label>
                     <input className="radio" type="radio" id={option1} name="radio" value={option1} onClick={() => setAnswer(option1)}/>
-                    <div className="btn btn-outline-primary btn-option">{option1}</div>
+                    <div className={optionClasses}>{option1}</div>
                 </label>
                 <label>
                     <input className="radio" type="radio" id={option2} name="radio" value={option2} onClick={() => setAnswer(option2)}/>
-                    <div className="btn btn-outline-primary btn-option">{option2}</div>
+                    <div className={optionClasses}>{option2}</div>
                 </label>
                 <label>
                     <input className="radio" type="radio" id={option3} name="radio" value={option3} onClick={() => setAnswer(option3)}/>
-                    <div className="btn btn-outline-primary btn-option">{option3}</div>
+                    <div className={optionClasses}>{option3}</div>
                 </label>
             </form>
         </div>
